1b0eff8390d84e840a0b5b18de8ab9f48ce8a5f6
ALEXIOS PETROU <122560620+alexispetrou2004@users.noreply.github.com>
2025-06-07 18:26:11 +0300

    added code to user.java

--- backend/src/main/java/com/example/users/user.java
+++ backend/src/main/java/com/example/users/user.java
@@ -1,5 +1,66 @@
 package com.example.users;
 
 public class user {
-    
+    private String username;
+    private String role;
+    private String firstName; 
+    private String lastName; 
+    private  String id;
+
+    public user(String username, String role, String firstName, String lastName, String id) {
+        this.username = username;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+ 
+
+    @Override
+    public String toString() {
+        return "User{" +
+               "username='" + username + '\'' +
+               ", role='" + role + '\'' +
+               ", firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", id='" + id + '\'' +
+               '}';
+    }
 }
