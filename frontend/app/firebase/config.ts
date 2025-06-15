da76dcf7ace0573bfeea8abd1927e6905d7396b6
Efthimismar <efthimismarkogiannis72@hotmail.com>
2025-06-15 16:03:05 +0300

    adding user's house managing

--- frontend/app/firebase/config.ts
+++ frontend/app/firebase/config.ts
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,7 +23,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-// const analytics = getAnalytics(app);
-
-export { app, auth, analytics };
+export const db = getFirestore(app);
+export const storage = getStorage(app);
